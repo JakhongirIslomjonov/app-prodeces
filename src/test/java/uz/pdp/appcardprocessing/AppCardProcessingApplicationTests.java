98f71feda31c7722788b4db2b4e8a51c71190142
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-10-17 11:33:26 +0500

    first commit

---
+++ src/test/java/uz/pdp/appcardprocessing/AppCardProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.appcardprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCardProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
